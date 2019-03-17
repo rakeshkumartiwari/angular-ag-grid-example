af884cc454b66ac69e64d8af8e32dfd528410f81
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2019-03-18 01:50:33 +0530

    bind data with json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { AgGridNg2 } from 'ag-grid-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,47 +9,87 @@ import { AgGridNg2 } from 'ag-grid-angular';
 })
 export class AppComponent {
   title = 'Ag Grid Example';
+  private gridApi;
+  private gridColumnApi;
+  private columnDefs;
+  private sortingOrder;
+
+
   @ViewChild('agGrid') agGrid: AgGridNg2;
-  column = [
-    {
-      checkboxSelection: true,
-      'headerName': 'Name',
-      'field': 'name'
-    },
-    {
-      'headerName': 'Mobile',
-      'field': 'mobile'
-    },
-    {
-      'headerName': 'State',
-      'field': 'state'
-    }
-  ];
 
-  row = [
-    {
-      'name': 'Rakesh',
-      'mobile': 7797977979,
-      'state': 'Telangana'
-    },
-    {
-      'name': 'Aniket',
-      'mobile': 798798979,
-      'state': 'Telangana'
-    },
-    {
-      'name': 'Raghu',
-      'mobile': 7987987888,
-      'state': 'Telangana'
-    }
-  ];
+  constructor(private http: HttpClient) {
 
-  getSelectedRows() {
-    const selectedNodes = this.agGrid.api.getSelectedNodes();
-    const selectedData = selectedNodes.map( node => node.data );
-    const selectedDataStringPresentation = selectedData
-    .map( node => node.name + ' ' + node.mobile + ' ' + node.state).join(', ');
-    alert(`Selected nodes: ${selectedDataStringPresentation}`);
+    this.columnDefs = [
+      {
+        headerName: 'Athlete',
+        field: 'athlete',
+        width: 150
+      },
+      {
+        headerName: 'Age',
+        field: 'age',
+        width: 100
+      },
+      {
+        headerName: 'Country',
+        field: 'country',
+        width: 100
+      },
+      {
+        headerName: 'Year',
+        field: 'year',
+        width: 100
+      },
+      {
+        headerName: 'Date',
+        field: 'date',
+        width: 100
+      },
+      {
+        headerName: 'Sport',
+        field: 'sport',
+        width: 150
+      },
+      {
+        headerName: 'Gold',
+        field: 'gold',
+        width: 100
+      },
+      {
+        headerName: 'Silver',
+        field: 'silver',
+        width: 100
+      },
+      {
+        headerName: 'Bronze',
+        field: 'bronze',
+        width: 100
+      },
+      {
+        headerName: 'Total',
+        field: 'total',
+        width: 100
+      }
+    ];
+  }
+ 
+  // getSelectedRows() {
+  //   const selectedNodes = this.agGrid.api.getSelectedNodes();
+  //   const selectedData = selectedNodes.map(node => node.data);
+  //   const selectedDataStringPresentation = selectedData
+  //     .map(node => node.name + ' ' + node.mobile + ' ' + node.state).join(', ');
+  //   alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  // }
+
+  onGridReddy(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+    this.http
+      .get('https://raw.githubusercontent.com/ag-grid/ag-grid-docs/master/src/olympicWinnersSmall.json')
+      .subscribe(data => {
+        params.api.setRowData(data);
+      });
+
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import{AgGridModule} from 'ag-grid-angular';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, AgGridModule.withComponents([])
+    BrowserModule, AgGridModule.withComponents([]), HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
